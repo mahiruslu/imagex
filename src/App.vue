235d28b650fef36b808a5716d31137023c6de723
Mahir <mhrsl07@gmail.com>
2021-10-28 23:40:33 +0300

    Api's added to source.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,55 @@
 <template>
-  <card msg="bu bir card"/>
+  <card :imgUrl="this.imgUrl" :description="this.description" :explanation="this.explanation"/>
 </template>
 
 <script>
 import card from './components/card.vue'
-
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
     card
+  },
+  data(){
+    return{
+      accessKey:'Rap3jucUeb35yLQQLOnBa_dOpylxLe8v8eovS-7TXY4',
+      url:'https://api.unsplash.com/photos/random',
+      imgUrl:'',
+      description:'hello',
+      explanation:'deneme',
+    }
+  },
+  methods:{
+    async getImages() {
+      try{
+        const res = await axios.get(this.url + `?client_id=${this.accessKey}`)
+        console.log(res);
+        this.imgUrl = res.data.urls.small;
+      }catch(err){
+        console.log(err);
+      }
+    },
+    async getDatas(){
+      try{
+        const emptyDate = new Date();
+        const randomDate = new Date();
+        const dateFormatter = Intl.DateTimeFormat('sv-SE');
+        const formattedRandomDate = dateFormatter.format(emptyDate.setDate(randomDate.getDate() - Math.floor(Math.random()*1000)));
+        
+        console.log(formattedRandomDate);
+        const res = await axios.get('https://api.nasa.gov/planetary/apod?api_key=JZbsf02DnpW3BT4aF2e49Y0qotXS1cI7THtfKswP'+`&date=${formattedRandomDate}`)
+        console.log(res.data);
+        this.description = res.data.copyright;
+        this.explanation = res.data.explanation;
+      }catch(err){
+        console.log(err);
+      }
+    }
+  
+  },
+  created(){
+    this.getImages();
+    this.getDatas();
   }
 }
 </script>
@@ -21,5 +62,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 100%;
 }
 </style>
